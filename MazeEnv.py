a31b4afcd949cc91eb18b96e11c1bc4256f9cd10
NUAA-YANG <2997357856@qq.com>
2023-08-29 19:31:46 +0800

    添加所有注释

--- MazeEnv.py
+++ MazeEnv.py
@@ -1,64 +1,63 @@
 '''
 @Author ：YZX
-@Date ：2023/8/7 15:44 
+@Date ：2023/8/7 16:03
 @Python-Version ：3.8
 '''
 
 import tkinter as tk
-import sys
 import numpy as np
-
+ 
 UNIT = 40  # pixels 像素
 MAZE_H = 4  # grid height y轴格子数
 MAZE_W = 4  # grid width x格子数
-
-
+ 
 # 迷宫
 class Maze(tk.Tk, object):
     def __init__(self):
         print("<env init>")
         super(Maze, self).__init__()
-
+ 
         # 动作空间(定义智能体可选的行为),action=0-3
         self.action_space = ['u', 'd', 'l', 'r']
         # 使用变量
         self.n_actions = len(self.action_space)
         # 状态空间，state=0,1
         self.n_states = 2
-
+ 
         # 配置信息
         self.title('maze')
         # 设置屏幕大小
         self.geometry("160x160")
-
+ 
         # 初始化操作
         self.__build_maze()
-
+ 
     # 渲染画面
     def render(self):
         # time.sleep(0.1)
         self.update()
-
+ 
     # 重置环境
     def reset(self):
         # 智能体回到初始位置
         # time.sleep(0.1)
         self.update()
         self.canvas.delete(self.rect)
-
+ 
+ 
         origin = np.array([20, 20])
-
+ 
         # 智能体位置，前两个左上角坐标(x0,y0)，后两个右下角坐标(x1,y1)
         self.rect = self.canvas.create_rectangle(
             origin[0] - 15, origin[1] - 15,
             origin[0] + 15, origin[1] + 15,
             fill='red')
-
+ 
         # return observation 状态
         # canvas.coords(长方形/椭圆),会得到 【左极值点、上极值点、右极值点、下极值点】这四个点组成的元组，:2表示前2个
-        return (np.array(self.canvas.coords(self.rect)[:2]) - np.array(self.canvas.coords(self.oval)[:2])) / (
-                    MAZE_H * UNIT)
-
+        return (np.array(self.canvas.coords(self.rect)[:2]) - np.array(self.canvas.coords(self.oval)[:2])) / (MAZE_H * UNIT)
+ 
+ 
     # 智能体向前移动一步：返回next_state,reward,terminal
     def step(self, action):
         s = self.canvas.coords(self.rect)
@@ -75,11 +74,11 @@ class Maze(tk.Tk, object):
         elif action == 3:  # left
             if s[0] > UNIT:
                 base_action[0] -= UNIT
-
+ 
         self.canvas.move(self.rect, base_action[0], base_action[1])  # move agent
-
+ 
         next_coords = self.canvas.coords(self.rect)  # next state
-
+ 
         # reward function
         if next_coords == self.canvas.coords(self.oval):
             reward = 1
@@ -94,12 +93,12 @@ class Maze(tk.Tk, object):
             done = False
         s_ = (np.array(next_coords[:2]) - np.array(self.canvas.coords(self.oval)[:2])) / (MAZE_H * UNIT)
         return s_, reward, done
-
+ 
     def __build_maze(self):
         self.canvas = tk.Canvas(self, bg='white',
                                 height=MAZE_H * UNIT,
                                 width=MAZE_W * UNIT)
-
+ 
         # create grids
         for c in range(0, MAZE_W * UNIT, UNIT):
             x0, y0, x1, y1 = c, 0, c, MAZE_H * UNIT
@@ -126,3 +125,4 @@ class Maze(tk.Tk, object):
             origin[0] + 15, origin[1] + 15,
             fill='red')
         self.canvas.pack()
+ 
--- RL.py
+++ RL.py
@@ -1,3 +1,9 @@
+'''
+@Author ：YZX
+@Date ：2023/8/7 10:21
+@Python-Version ：3.8
+'''
+
 import torch
 # 用于构建神经网络的各种工具和类
 import torch.nn as nn
@@ -8,26 +14,26 @@ import matplotlib.pyplot as plt
  
 # 深度网络，全连接层
 class Net(nn.Module):
-    # 输入状态和动作
+    # 输入状态和动作，当前例子中状态有2个表示为坐标(x,y)，动作有4个表示为(上下左右)
     def __init__(self, n_states, n_actions):
         super(Net, self).__init__()
-        # 创建一个线性层，n_states行10列
+        # 创建一个线性层，2行10列
         self.fc1 = nn.Linear(n_states, 10)
-        # 创建一个线性层，10行n_actions列
+        # 创建一个线性层，10行4列
         self.fc2 = nn.Linear(10, n_actions)
-        # 随机初始化生成权重
+        # 随机初始化生成权重，范围是0-0.1
         self.fc1.weight.data.normal_(0, 0.1)
         self.fc2.weight.data.normal_(0, 0.1)
  
-    # 前向传播
-    def forward(self, x):
+    # 前向传播（用于状态预测动作的值）
+    def forward(self, state):
         # 这里以一个动作为作为观测值进行输入(输入张量)
         # 线性变化后输出给10个神经元，格式：(x,x,x,x,x,x,x,x,x,x,x)
-        x = self.fc1(x)
+        state = self.fc1(state)
         # 激活函数，将负值设置为零，保持正值不变
-        x = F.relu(x)
+        state = F.relu(state)
         # 经过10个神经元运算过后的数据，线性变化后把每个动作的价值作为输出。
-        out = self.fc2(x)
+        out = self.fc2(state)
         return out
  
 # 定义DQN网络class
@@ -47,9 +53,9 @@ class DQN:
  
         # 使用变量
         # 用来记录学习到第几步了
-        self.learn_step_counter = 0  # target网络学习计数
+        self.learn_step_counter = 0
         # 用来记录当前指到数据库的第几个数据了
-        self.memory_counter = 0  # 记忆计数
+        self.memory_counter = 0
         # 创建一个2000行6列的矩阵，即表示可存储2000行经验，每一行6个特征值
         # 2*2表示当前状态state(x,y)和下一个状态next_state(x,y) + 1表示选择一个动作 + 1表示一个奖励值
         self.memory = np.zeros((2000, 2 * 2 + 1 + 1))
@@ -57,20 +63,20 @@ class DQN:
         self.steps_of_each_episode = []  # 记录每轮走的步数
  
     # 进行选择动作
-    # x是state= [-0.5 -0.5]
-    def choose_action(self, x, epsilon):
+    # state = [-0.5 -0.5]
+    def choose_action(self, state, epsilon):
         # 扩展一行,因为网络是多维矩阵,输入是至少两维
         # torch.FloatTensor(x)先将x转化为浮点数张量
         # torch.unsqueeze(input, dim)再将一维的张量转化为二维的,dim=0时数据为行方向扩，dim=1时为列方向扩
         # 例如 [1.0, 2.0, 3.0] -> [[1.0, 2.0, 3.0]]
-        x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        state = torch.unsqueeze(torch.FloatTensor(state), 0)
         # 在大部分情况，我们选择 去max-value
         if np.random.uniform() < epsilon:   # greedy # 随机结果是否大于EPSILON（0.9）
             # 获取动作对应的价值
-            action_value = self.eval_net.forward(x)
+            action_value = self.eval_net.forward(state)
             #   torch.max() 返回输入张量所有元素的最大值，torch.max(input, dim)，dim是max函数索引的维度0/1，0是每列的最大值，1是每行的最大值
             #   torch.max(a, 1)[1] 代表a中每行最大值的索引
-            # .data.numpy()[0]将Variable转换成tensor
+            #   data.numpy()[0] 将Variable转换成tensor
             # 哪个神经元值最大，则代表下一个动作
             action = torch.max(action_value, 1)[1].data.numpy()[0]
         # 在少部分情况，我们选择 随机选择 （变异）
@@ -84,12 +90,12 @@ class DQN:
     # 存储【本次状态，执行的动作，获得的奖励分，完成动作后产生的下一个状态】
     def store_transition(self, state, action, reward, next_state):
         # 把所有的记忆捆在一起，以 np 类型
-        # 把 三个矩阵 s ,[a,r] ,s_  平铺在一行 [a,r]是因为 他们都是 int 没有 [] 就无法平铺 ，并不代表把他们捆在一起了
+        # 把 三个矩阵 s ,[a,r] ,s_  平铺在一行 [a,r] 是因为 他们都是 int 没有 [] 就无法平铺 ，并不代表把他们捆在一起了
         #  np.hstack()是把矩阵按水平方向堆叠数组构成一个新的数组
         transition = np.hstack((state, [action, reward], next_state))
         # index 是 这一次录入的数据在 MEMORY_CAPACITY 的哪一个位置
         # 如果记忆超过上线，我们重新索引。即覆盖老的记忆。
-        index = self.memory_counter % 2000
+        index = self.memory_counter % 200
         self.memory[index, :] = transition  # 将transition添加为memory的一行
         self.memory_counter += 1
 
@@ -97,18 +103,18 @@ class DQN:
     # 从存储学习数据
     # target_net是达到次数后更新， eval_net是每次learn就进行更新
     def learn(self):
-        # 更新 target_net
+        # 更新 target_net，每循环100次更新一次
         if self.learn_step_counter % 100 == 0:
             # 将评估网络的参数状态复制到目标网络中
             # 即将target_net网络变成eval_net网络，实现模型参数的软更新
             self.target_net.load_state_dict((self.eval_net.state_dict()))
         self.learn_step_counter += 1
- 
-        # eval net是 每次learn 就进行更新
+
+        # eval_net是 每次 learn 就进行更新
         # 从[0,200)中随机抽取16个数据并组成一维数组，该数组表示记忆索引值
         sample_index = np.random.choice(200, 16)
         # 表示从 self.memory 中选择索引为 sample_index 的行，: 表示选取所有列
-        # 即得到16组随机选取的训练样本。
+        # 按照随机获得的索引值获取对应的记忆数据
         memory = self.memory[sample_index, :]
         # 从记忆当中获取[0,2)列，即第零列和第一列，表示状态特征
         state = torch.FloatTensor(memory[:, :2])
@@ -119,7 +125,7 @@ class DQN:
         # 从记忆中获取[4,5)列，即第四列和第五列，表示下一个状态特征
         next_state = torch.FloatTensor(memory[:, 4:6])
 
-        # 获得当前状态的动作对应的预测Q值
+        # 从原来的网络中获得当前状态的动作对应的预测Q值
         # self.eval_net(state)表示输入当前state，通过forward()函数输出状态对应的Q值估计
         # .gather(1, action)表示从上述Q值估计的集合中，第一个维度上获取action对应的的Q值
         # 将Q值赋值给q_eval，表示所采取动作的预测value
@@ -137,6 +143,7 @@ class DQN:
 
         # 通过预测值与真实值计算损失 q_eval预测值， q_target真实值
         loss = self.loss(q_eval, q_target)
+        # 记录损失值
         self.cost.append(loss.detach().numpy())
         # 根据误差，去优化我们eval网, 因为这是eval的优化器
         # 反向传递误差，进行参数更新
